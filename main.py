a0c84aa048ecc2e45939ce69d7cc3b41117dc81a
s3nh <steam.panek@gmail.com>
2020-01-13 21:05:28 +0100

    Idea

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+BlobMix implementation, 
+
+robust solution to state of the art classifiers. 
+"""
+import numpy as np 
+import matplotlib.pyplot as plt 
+import os 
+import sys 
+import re 
+import json
+
+class Blob(object):
+    def __init__(self, ratio, shape):
+        """
+        Blob ratio from uniform distribution
+        ratio based on images dataset
+
+
+        """
+        self.ratio = ratio
+        self.shape = shape 
+
+    def __call_(self):
+        pass
+
+    def __getitem__(self):
+        raise NotImplementedError
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
